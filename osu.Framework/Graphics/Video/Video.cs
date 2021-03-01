15d83ff9620d1292c3fe551fd9e920894215e57f
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2021-03-01 15:18:36 +0100

    Check what video decoding options are available in the build

--- osu.Framework/Graphics/Video/Video.cs
+++ osu.Framework/Graphics/Video/Video.cs
@@ -1,24 +1,27 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
+using FFmpeg.AutoGen;
 using JetBrains.Annotations;
+using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics.Animations;
+using osu.Framework.Graphics.Shaders;
 using osu.Framework.Logging;
 using osu.Framework.Platform;
-using osu.Framework.Graphics.Shaders;
 using osuTK;
+using AGffmpeg = FFmpeg.AutoGen.ffmpeg;
 
 namespace osu.Framework.Graphics.Video
 {
     /// <summary>
     /// Represents a composite that displays a video played back from a stream or a file.
     /// </summary>
-    public class Video : AnimationClockComposite
+    public unsafe class Video : AnimationClockComposite
     {
         /// <summary>
         /// Whether this video is in a buffering state, waiting on decoder or underlying stream.
@@ -93,6 +96,8 @@ namespace osu.Framework.Graphics.Video
 
         public override Drawable CreateContent() => Sprite = new VideoSprite(this) { RelativeSizeAxes = Axes.Both };
 
+        private readonly FFmpegFuncs ffmpeg;
+
         /// <summary>
         /// Creates a new <see cref="Video"/>.
         /// </summary>
@@ -102,6 +107,15 @@ namespace osu.Framework.Graphics.Video
             : base(startAtCurrentTime)
         {
             this.stream = stream ?? throw new ArgumentNullException(nameof(stream));
+            ffmpeg = CreateFuncs();
+            var type = AVHWDeviceType.AV_HWDEVICE_TYPE_NONE;
+            var availableHWDecoders = new Dictionary<int, AVHWDeviceType>();
+            var i = 0;
+            while ((type = ffmpeg.av_hwdevice_iterate_types(type)) != AVHWDeviceType.AV_HWDEVICE_TYPE_NONE)
+            {
+                availableHWDecoders.Add(i, type);
+                Logger.Log($"{++i}. {type}");
+            }
         }
 
         [BackgroundDependencyLoader]
@@ -195,5 +209,71 @@ namespace osu.Framework.Graphics.Video
 
         protected override Vector2 GetCurrentDisplaySize() =>
             new Vector2(Sprite.Texture?.DisplayWidth ?? 0, Sprite.Texture?.DisplayHeight ?? 0);
+
+        protected virtual FFmpegFuncs CreateFuncs()
+        {
+            // other frameworks should handle native libraries themselves
+#if NET5_0
+            AGffmpeg.GetOrLoadLibrary = name =>
+            {
+                int version = AGffmpeg.LibraryVersionMap[name];
+
+                string libraryName = null;
+
+                // "lib" prefix and extensions are resolved by .net core
+                switch (RuntimeInfo.OS)
+                {
+                    case RuntimeInfo.Platform.MacOsx:
+                        libraryName = $"{name}.{version}";
+                        break;
+
+                    case RuntimeInfo.Platform.Windows:
+                        libraryName = $"{name}-{version}";
+                        break;
+
+                    case RuntimeInfo.Platform.Linux:
+                        libraryName = name;
+                        break;
+                }
+
+                return NativeLibrary.Load(libraryName, System.Reflection.Assembly.GetEntryAssembly(), DllImportSearchPath.UseDllDirectoryForDependencies | DllImportSearchPath.SafeDirectories);
+            };
+#endif
+
+            return new FFmpegFuncs
+            {
+                av_frame_alloc = AGffmpeg.av_frame_alloc,
+                av_frame_free = AGffmpeg.av_frame_free,
+                av_frame_unref = AGffmpeg.av_frame_unref,
+                av_frame_get_buffer = AGffmpeg.av_frame_get_buffer,
+                av_strdup = AGffmpeg.av_strdup,
+                av_strerror = AGffmpeg.av_strerror,
+                av_malloc = AGffmpeg.av_malloc,
+                av_packet_alloc = AGffmpeg.av_packet_alloc,
+                av_packet_unref = AGffmpeg.av_packet_unref,
+                av_packet_free = AGffmpeg.av_packet_free,
+                av_read_frame = AGffmpeg.av_read_frame,
+                av_seek_frame = AGffmpeg.av_seek_frame,
+                avcodec_find_decoder = AGffmpeg.avcodec_find_decoder,
+                avcodec_open2 = AGffmpeg.avcodec_open2,
+                avcodec_receive_frame = AGffmpeg.avcodec_receive_frame,
+                avcodec_send_packet = AGffmpeg.avcodec_send_packet,
+                avformat_alloc_context = AGffmpeg.avformat_alloc_context,
+                avformat_close_input = AGffmpeg.avformat_close_input,
+                avformat_find_stream_info = AGffmpeg.avformat_find_stream_info,
+                avformat_open_input = AGffmpeg.avformat_open_input,
+                avio_alloc_context = AGffmpeg.avio_alloc_context,
+                sws_freeContext = AGffmpeg.sws_freeContext,
+                sws_getContext = AGffmpeg.sws_getContext,
+                sws_scale = AGffmpeg.sws_scale,
+                av_hwdevice_iterate_types = AGffmpeg.av_hwdevice_iterate_types,
+                av_hwdevice_ctx_create = AGffmpeg.av_hwdevice_ctx_create,
+                av_find_best_stream = AGffmpeg.av_find_best_stream,
+                avcodec_alloc_context3 = AGffmpeg.avcodec_alloc_context3,
+                av_hwframe_transfer_data = AGffmpeg.av_hwframe_transfer_data,
+                avcodec_parameters_to_context = AGffmpeg.avcodec_parameters_to_context,
+                avcodec_flush_buffers = AGffmpeg.avcodec_flush_buffers
+            };
+        }
     }
 }
